ca247d1018de4cdbb20b0f73aa954f7b12b7f350
Dzianisava_KV <Dzianisava_KV@st.by>
2019-04-25 16:47:35 +0300

    init RX

--- app/src/main/java/com/example/currency/MainActivity.java
+++ app/src/main/java/com/example/currency/MainActivity.java
@@ -4,13 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import java.util.ArrayList;
+import org.simpleframework.xml.convert.AnnotationStrategy;
+import org.simpleframework.xml.core.Persister;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
 import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,22 +31,36 @@ public class MainActivity extends AppCompatActivity {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(URL_RASES)
                 .client(new OkHttpClient())
-                .addConverterFactory(SimpleXmlConverterFactory.create())
+                .addConverterFactory(SimpleXmlConverterFactory.createNonStrict(new Persister(new AnnotationStrategy())))
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
 
         RatesAPI api = retrofit.create(RatesAPI.class);
+        api.currencies()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new DisposableSingleObserver <Currencies>() {
+                    @Override
+                    public void onSuccess(Currencies currencies) {
+                        Log.d("tag", "onSuccess: "+ currencies);
+                    }
 
-        Call<Currencies> call = api.getCurrencies();
-        call.enqueue(new Callback<Currencies>() {
-            @Override
-            public void onResponse(Call<Currencies> call, Response<Currencies> response) {
-                Log.d("tag", "onResponse: "+ response.body());
-            }
-
-            @Override
-            public void onFailure(Call<Currencies> call, Throwable t) {
-                t.printStackTrace();
-            }
-        });
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                    }
+                });
+//        Call<Currencies> call = api.getCurrencies();
+//        call.enqueue(new Callback<Currencies>() {
+//            @Override
+//            public void onResponse(Call<Currencies> call, Response<Currencies> response) {
+//                Log.d("tag", "onResponse: "+ response.body());
+//            }
+//
+//            @Override
+//            public void onFailure(Call<Currencies> call, Throwable t) {
+//                t.printStackTrace();
+//            }
+//        });
     }
 }
--- app/src/main/java/com/example/currency/RatesAPI.java
+++ app/src/main/java/com/example/currency/RatesAPI.java
@@ -1,12 +1,18 @@
 package com.example.currency;
 
+import java.util.List;
+
+import io.reactivex.Single;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface RatesAPI  {
-    @GET("XmlExRates.aspx?ondate=04.25.2019")
-    Call<Currencies> getCurrencies();
+//    @GET("XmlExRates.aspx?ondate=04.25.2019")
+//    Call<Currencies> getCurrencies();
 
     @GET("XmlExRates.aspx?ondate=04.25.2019")
     Call<String> getRates();
+
+    @GET("XmlExRates.aspx?ondate=04.25.2019")
+    Single<Currencies> currencies();
 }
